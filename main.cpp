73d61fccd938007ea1e5f35b3577dfd886390952
davidVeer <davidvdveer05@gmail.com>
2025-12-01 14:07:35 +0100

    Copying a scene template

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <GLFW/glfw3.h>
 #include "tigl.h"
 #include <glm/gtc/matrix_transform.hpp>
+#include "CameraHandling/Headers/CameraStrategy.h"
+#include "CameraHandling/Headers/KeyboardAndMouse_Camera.h"
+
 using tigl::Vertex;
 
 #pragma comment(lib, "glfw3.lib")
@@ -9,6 +12,7 @@ using tigl::Vertex;
 #pragma comment(lib, "opengl32.lib")
 
 GLFWwindow* window;
+std::shared_ptr<ICameraStrategy> cameraControl;
 
 void init();
 void update();
@@ -52,17 +56,43 @@ void init()
         if (key == GLFW_KEY_ESCAPE)
             glfwSetWindowShouldClose(window, true);
     });
-
+    cameraControl = std::make_shared<KeyboardAndMouseCamera>(KeyboardAndMouseCamera(window));
 }
 
 
 void update()
 {
-
+    cameraControl->update(window);
 }
 
 void draw()
 {
+    glm::mat4 projectionMatrix, modelMatrix, viewMatrix;
+
     glClearColor(0.3f, 0.4f, 0.6f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnable(GL_DEPTH_TEST);
+
+    int viewport[4];
+    glGetIntegerv(GL_VIEWPORT, viewport);
+    projectionMatrix = glm::perspective(glm::radians(75.0f), viewport[2] / (float)viewport[3], 0.01f, 100.0f);
+
+    tigl::shader->setProjectionMatrix(projectionMatrix);
+    tigl::shader->setViewMatrix(cameraControl->getMatrix());
+
+    tigl::begin(GL_TRIANGLES);
+    tigl::addVertex(Vertex::PC(glm::vec3(-2, -1, -4), glm::vec4(1, 1, 1, 1))); // Bottom left
+    tigl::addVertex(Vertex::PC(glm::vec3(2, -1, -4), glm::vec4(0, 0, 0, 1))); // Bottom Right
+    tigl::addVertex(Vertex::PC(glm::vec3(0, 1, -4), glm::vec4(0.5, 0.5, 0.5, 1))); // Top
+
+
+    tigl::addVertex(Vertex::PC(glm::vec3(-10, -1, -10), glm::vec4(1, 1, 1, 1)));
+    tigl::addVertex(Vertex::PC(glm::vec3(-10, -1, 10), glm::vec4(1, 1, 1, 1)));
+    tigl::addVertex(Vertex::PC(glm::vec3(10, -1, 10), glm::vec4(1, 1, 1, 1)));
+
+    tigl::addVertex(Vertex::PC(glm::vec3(-10, -1, -10), glm::vec4(1, 1, 1, 1)));
+    tigl::addVertex(Vertex::PC(glm::vec3(10, -1, -10), glm::vec4(1, 1, 1, 1)));
+    tigl::addVertex(Vertex::PC(glm::vec3(10, -1, 10), glm::vec4(1, 1, 1, 1)));
+
+    tigl::end();
 }
